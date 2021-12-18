bbf5fb39167e1cc99a6729de65c66325a00643b8
Marian Weger <weger@hoplomachus.iem.at>
2021-12-18 17:36:59 +0100

    ?

--- tools/push_to_bela.sh
+++ tools/push_to_bela.sh
@@ -7,4 +7,6 @@ echo "pushing patch to bela (overwriting everything)..."
 # scp -r $script_path/../bela/* root@bela.local:~/Bela/projects/schroedingers_box
 rsync -avz $script_path/../bela/ root@bela.local:~/Bela/projects/schroedingers_box/
 
+rsync -avz $script_path/../SchroedingersUGens/ root@bela.local:/usr/local/share/SuperCollider/Extensions/SchroedingersUGens/
+
 echo "done."
