5377dcc3083c40c37b4a4aae2627f6a45f8abf84
Marian Weger <marian@secutor>
2021-12-19 14:39:16 +0100

    FIXES: Tuned Velocity Meaurement and Sound Localization, Tuned System Params for Low Latency, Tuned onset detection params, etc.

--- tools/push_to_bela.sh
+++ tools/push_to_bela.sh
@@ -5,8 +5,8 @@ script_path=$(dirname $(readlink -f $0))
 echo "pushing patch to bela (overwriting everything)..."
 
 # scp -r $script_path/../bela/* root@bela.local:~/Bela/projects/schroedingers_box
-rsync -avz $script_path/../bela/ root@bela.local:~/Bela/projects/schroedingers_box/
+rsync -avz --delete $script_path/../bela/ root@bela.local:~/Bela/projects/schroedingers_box/
 
-rsync -avz $script_path/../SchroedingersUGens/ root@bela.local:/usr/local/share/SuperCollider/Extensions/SchroedingersUGens/
+rsync -avz --delete $script_path/../SchroedingersUGens/ root@bela.local:/usr/local/share/SuperCollider/Extensions/SchroedingersUGens/
 
 echo "done."
