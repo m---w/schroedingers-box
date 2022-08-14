57b24e5f3bc0fc5479d682b79f27645731386dcb
Marian Weger <marian@secutor>
2022-08-14 19:11:23 +0200

    debugged on hardware. updated install scripts.

---
+++ tools/get_quarks_and_sources.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# shell script to clone git repositories of required SuperCollider Quarks
+# see also here: https://forum.bela.io/d/1711-quarks/3
+
+script_path=$(dirname $(readlink -f $0))
+
+echo "cloning sources..."
+
+mkdir -p $script_path/../bela/quarks
+cd $script_path/../bela/quarks
+
+if [ ! -d "ddwVoicer" ] ; then
+    git clone https://github.com/supercollider-quarks/ddwVoicer.git
+fi
+
+if [ ! -d "ddwCommon" ] ; then
+    git clone https://github.com/supercollider-quarks/ddwCommon.git
+fi
+
+if [ ! -d "ddwGUIEnhancements" ] ; then
+    git clone https://github.com/supercollider-quarks/ddwGUIEnhancements.git
+fi
+
+if [ ! -d "crucial-library" ] ; then
+    git clone https://github.com/supercollider-quarks/crucial-library.git
+fi
+
+
+mkdir -p $script_path/../src
+cd $script_path/../src
+
+if [ ! -d "supercollider" ] ; then
+    git clone https://github.com/supercollider/supercollider
+    git -C supercollider checkout a2e58ce6af14f573d26abdf5d56824b212aba205
+    # git -C supercollider checkout b8ddcaf0f89c019af0f682a49f5df1c516578067
+fi
+
+
+echo "done."
--- tools/push_to_bela.sh
+++ tools/push_to_bela.sh
@@ -9,4 +9,6 @@ rsync -avz --delete $script_path/../bela/ root@bela.local:~/Bela/projects/schroe
 
 rsync -avz --delete $script_path/../Extensions/ root@bela.local:~/.local/share/SuperCollider/Extensions
 
+rsync -avz $script_path/../src/supercollider/SCClassLibrary/* root@bela.local:/usr/share/SuperCollider/SCClassLibrary/
+
 echo "done."
