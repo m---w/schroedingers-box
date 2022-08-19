44a962c8ed90f3a86957f0e8418e54673368670a
Marian Weger <marian@secutor>
2022-08-19 22:15:52 +0200

    fixed quark install. ADD: red button via digital in.

--- tools/push_to_bela.sh
+++ tools/push_to_bela.sh
@@ -7,7 +7,7 @@ echo "pushing patch to bela (overwriting everything)..."
 # scp -r $script_path/../bela/* root@bela.local:~/Bela/projects/schroedingers_box
 rsync -avz --delete $script_path/../bela/ root@bela.local:~/Bela/projects/schroedingers_box/
 
-rsync -avz --delete $script_path/../install root@bela.local:~/Bela/projects/install/
+rsync -avz --delete $script_path/../install/ root@bela.local:~/Bela/projects/install/
 
 rsync -avz --delete $script_path/../Extensions/ root@bela.local:~/.local/share/SuperCollider/Extensions
 
